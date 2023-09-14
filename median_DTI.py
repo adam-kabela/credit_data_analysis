f9d1f79edb377a89e5ef55510ea68f98a1881b08
adam-kabela <adam.kabela@gmail.com>
2023-09-14 17:39:09 +0200

    Bug fix. Must use merge(...,  how = 'left') to consider all applications.

--- median_DTI.py
+++ median_DTI.py
@@ -8,6 +8,7 @@ import pandas
 application_columns = ['SK_ID_CURR','AMT_INCOME_TOTAL','AMT_CREDIT','NAME_INCOME_TYPE']
 application = pandas.read_csv('data/application_train.csv', usecols = application_columns)
 #print(application.head(10)) # see a few rows to get a basic idea 
+print("Number of applicaions:", len(application.index))
 
 #is AMT_INCOME_TOTAL the client's annual income (or is it monthly and should by multiplied by 12?)
 #credit_card_balance_columns = ['SK_ID_CURR','MONTHS_BALANCE','AMT_BALANCE','AMT_DRAWINGS_CURRENT','AMT_PAYMENT_TOTAL_CURRENT','AMT_TOTAL_RECEIVABLE']
@@ -16,7 +17,7 @@ application = pandas.read_csv('data/application_train.csv', usecols = applicatio
 #spent_and_income = spent_and_income[spent_and_income['AMT_BALANCE'] > 0]
 #spent_and_income = spent_and_income.sort_values(['SK_ID_CURR','MONTHS_BALANCE'])
 #spent_and_income.to_csv('amounts_to_maybe_look_at.csv')
-# ok, AMT_INCOME_TOTAL is annual. This conclusion is based on this look, the resulting DMI values and googling...
+#ok, AMT_INCOME_TOTAL is annual. This conclusion is based on this look, the resulting DTI values and googling...
 
 # we suppose that all items have the same CREDIT_CURRENCY
 bureau_columns = ['SK_ID_CURR','CREDIT_ACTIVE','AMT_CREDIT_SUM']
@@ -24,11 +25,18 @@ bureau = pandas.read_csv('data/bureau.csv', usecols = bureau_columns)
 
 active_credits = bureau[bureau['CREDIT_ACTIVE'] == 'Active']
 active_credits_summed = active_credits.groupby('SK_ID_CURR')['AMT_CREDIT_SUM'].sum().reset_index(name ='CREDIT_TOTAL')
-all_info_merged = pandas.merge(application, active_credits_summed, on ='SK_ID_CURR')
+incomplete_all_info_merged = pandas.merge(application, active_credits_summed, on ='SK_ID_CURR')
+all_info_merged = pandas.merge(application, active_credits_summed, on ='SK_ID_CURR', how = 'left')
+print("The wrong merge covered only:", len(incomplete_all_info_merged.index))
+print("The left merge covers all:", len(all_info_merged.index))
+#print(incomplete_all_info_merged.head(10))
+#print(all_info_merged.head(10))
 
 # add a new column DEBT_TO_INCOME = (CREDIT_TOTAL + AMT_CREDIT)/client's annual income
+# if CREDIT_TOTAL is NaN, the sum treats it as 0
 to_be_summed = ['CREDIT_TOTAL', 'AMT_CREDIT'] 
 all_info_merged['DEBT_TO_INCOME'] = all_info_merged[to_be_summed].sum(axis=1) / all_info_merged['AMT_INCOME_TOTAL']
+#print(all_info_merged.head(10))
 
 output = all_info_merged.groupby('NAME_INCOME_TYPE').agg({'DEBT_TO_INCOME': ['mean', 'size']})
 output.columns = ['MEDIAN_DEBT_TO_INCOME', 'SAMPLE_SIZE']
